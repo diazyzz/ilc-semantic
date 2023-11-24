a8c58434ae8d5bde0d69f25896f38e54e594816d
diazyzz <diassnorrman@gmail.com>
2023-11-24 11:17:07 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,31 @@
-$( document ).ready(function() 
-{
-    $('a[href*="#"]').click(function(event) 
-    {
-        if 
-        (
-            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-            location.hostname == this.hostname
-        ) 
-        {
-            var object = $(this.hash);
-            object = object.length ? object : $('[name=' + this.hash.slice(1) + ']');
-            if (object.length)
-            {
-                event.preventDefault();
-                $('html, body').animate({
-                    scrollTop: object.offset().top
-                }, 1000, function()
-                {
-                    var $object = $(object);
-                    $object.focus();
-                    if ($object.is(":focus")) 
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        $object.attr('tabindex', '-1');
-                        $object.focus();
-                    };
-                });
+$(document).ready(function () {
+  $('a[href*="#"]').click(function (event) {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var object = $(this.hash);
+      object = object.length ? object : $("[name=" + this.hash.slice(1) + "]");
+      if (object.length) {
+        event.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: object.offset().top,
+          },
+          1000,
+          function () {
+            var $object = $(object);
+            $object.focus();
+            if ($object.is(":focus")) {
+              return false;
+            } else {
+              $object.attr("tabindex", "-1");
+              $object.focus();
             }
-        }
-    });
+          }
+        );
+      }
+    }
+  });
 });
